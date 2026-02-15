aa7629740950db9035d2c5a61f8faa56183d85ef
tangwz <tangwz.com@gmail.com>
2026-02-15 12:52:02 +0800

    redesign app icon and regenerate tauri assets

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,5 +1,6 @@
 fn main() {
     println!("cargo:rerun-if-changed=tauri.conf.json");
+    println!("cargo:rerun-if-changed=icons/app-icon.svg");
     println!("cargo:rerun-if-changed=icons/icon.png");
     tauri_build::build()
 }
